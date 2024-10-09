f8d277a2c6c94d96c2073980a32350490dbc32b3
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2024-10-09 17:18:33 -0500

    pre-day 5 refactoring

--- src/bin/02.rs
+++ src/bin/02.rs
@@ -1,70 +1,11 @@
-use advent_of_code::parse_usize;
-use nom::{bytes::complete::tag, multi::separated_list1, IResult};
+use advent_of_code::{execute, parse_machine};
 
 use itertools::Itertools;
 
 advent_of_code::solution!(2);
 
-#[derive(Debug, Clone)]
-struct IntcodeMachine {
-    program: Vec<usize>,
-    instruction_pointer: usize,
-}
-
-fn parser(i: &str) -> IResult<&str, Vec<usize>> {
-    separated_list1(tag(","), parse_usize)(i)
-}
-
-fn step(machine: &mut IntcodeMachine) -> bool {
-    let current_instruction = machine.program.get(machine.instruction_pointer);
-    if let Some(opcode) = current_instruction {
-        match opcode {
-            1 => {
-                let addition = machine
-                    .program
-                    .get(machine.instruction_pointer..=machine.instruction_pointer + 3)
-                    .expect("opcode read out of bounds")
-                    .to_owned();
-                machine.program[addition[3]] =
-                    machine.program[addition[1]] + machine.program[addition[2]];
-            }
-            2 => {
-                let multiplication = machine
-                    .program
-                    .get(machine.instruction_pointer..=machine.instruction_pointer + 3)
-                    .expect("opcode read out of bounds")
-                    .to_owned();
-                machine.program[multiplication[3]] =
-                    machine.program[multiplication[1]] * machine.program[multiplication[2]];
-            }
-            99 => {
-                return false;
-            }
-            _ => panic!("unknown opcode: {}", opcode),
-        }
-    } else {
-        panic!("instruction pointer out of bounds")
-    }
-    machine.instruction_pointer += 4;
-    // still active
-    true
-}
-
-fn execute(machine: &mut IntcodeMachine) {
-    loop {
-        let active = step(machine);
-        if !active {
-            break;
-        }
-    }
-}
-
-pub fn part_one(input: &str) -> Option<usize> {
-    let (_, program) = parser(input).unwrap();
-    let mut machine = IntcodeMachine {
-        program,
-        instruction_pointer: 0,
-    };
+pub fn part_one(input: &str) -> Option<i32> {
+    let (_, mut machine) = parse_machine(input).unwrap();
 
     machine.program[1] = 12;
     machine.program[2] = 2;
@@ -75,18 +16,14 @@ pub fn part_one(input: &str) -> Option<usize> {
 }
 
 pub fn part_two(input: &str) -> Option<usize> {
-    let (_, program) = parser(input).unwrap();
-    let machine = IntcodeMachine {
-        program,
-        instruction_pointer: 0,
-    };
+    let (_, machine) = parse_machine(input).unwrap();
 
     // can't run off the end of the program
     let cap = machine.program.len() - 1;
     for (noun, verb) in (0..=cap).cartesian_product(0..=cap) {
         let mut trial = machine.clone();
-        trial.program[1] = noun;
-        trial.program[2] = verb;
+        trial.program[1] = noun.try_into().unwrap();
+        trial.program[2] = verb.try_into().unwrap();
 
         execute(&mut trial);
 
@@ -105,11 +42,7 @@ mod tests {
     #[test]
     // the first test involved replacing values which breaks the tests if used directly
     fn test_part_one() {
-        let (_, program) = parser(&advent_of_code::template::read_file("examples", DAY)).unwrap();
-        let mut machine = IntcodeMachine {
-            program,
-            instruction_pointer: 0,
-        };
+        let (_, mut machine) = parse_machine(&advent_of_code::template::read_file("examples", DAY)).unwrap();
 
         execute(&mut machine);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,10 @@
-use nom::{character::complete::digit1, combinator::map_res, IResult};
+use nom::{
+    bytes::complete::tag,
+    character::complete::{digit1, i32},
+    combinator::map_res,
+    multi::separated_list1,
+    IResult,
+};
 
 use std::str::FromStr;
 pub mod template;
@@ -13,4 +19,77 @@ pub fn parse_usize(i: &str) -> IResult<&str, usize> {
 pub struct CoordinateSigned {
     pub x: i32,
     pub y: i32,
+}
+
+#[derive(Debug, Clone)]
+pub struct IntcodeMachine {
+    pub program: Vec<i32>,
+    pub instruction_pointer: usize,
+}
+
+pub fn parse_machine(i: &str) -> IResult<&str, IntcodeMachine> {
+    let (i, program) = separated_list1(tag(","), i32)(i)?;
+
+    Ok((
+        i,
+        IntcodeMachine {
+            program,
+            instruction_pointer: 0,
+        },
+    ))
+}
+
+fn step(machine: &mut IntcodeMachine) -> bool {
+    macro_rules! position {
+        ($n:expr) => {
+            machine
+                .program
+                .get_mut($n as usize)
+                .unwrap_or_else(|| panic!("value {} invalid index", $n))
+        };
+    }
+    macro_rules! command {
+        ($r:expr) => {
+            machine
+                .program
+                .get(machine.instruction_pointer..=machine.instruction_pointer + $r)
+                .expect("opcode read out of bounds")
+                .to_owned()
+        };
+    }
+
+    let current_instruction = machine.program.get(machine.instruction_pointer);
+    if let Some(opcode) = current_instruction {
+        match opcode {
+            1 => {
+                let addition = command!(4);
+                *position!(addition[3]) = *position!(addition[1]) + *position!(addition[2]);
+                machine.instruction_pointer += 4;
+            }
+            2 => {
+                let multiplication = command!(4);
+                *position!(multiplication[3]) =
+                    *position!(multiplication[1]) * *position!(multiplication[2]);
+                machine.instruction_pointer += 4;
+            }
+            99 => {
+                // command!(1);
+                return false;
+            }
+            _ => panic!("unknown opcode: {}", opcode),
+        }
+    } else {
+        panic!("instruction pointer out of bounds")
+    }
+    // still active
+    true
+}
+
+pub fn execute(machine: &mut IntcodeMachine) {
+    loop {
+        let active = step(machine);
+        if !active {
+            break;
+        }
+    }
 }

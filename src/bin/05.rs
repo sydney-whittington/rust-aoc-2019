f7de246331512b3116ceb0fe7cfa79ab1e55c827
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2024-10-15 12:14:05 -0500

    day 5 revision to work with day 7 refactoring for debugging

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -2,18 +2,23 @@ use advent_of_code::{execute, parse_machine};
 
 advent_of_code::solution!(5);
 
-pub fn part_one(input: &str) -> Option<u32> {
+pub fn part_one(input: &str) -> Option<i32> {
     let (_, mut machine) = parse_machine(input).unwrap();
 
+    machine.inputs.push_back(1);
     execute(&mut machine);
-    None
+    dbg!(&machine.outputs);
+
+    // last value is the diagnostic code
+    machine.outputs.pop_back()
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<i32> {
     let (_, mut machine) = parse_machine(input).unwrap();
 
+    machine.inputs.push_back(5);
     execute(&mut machine);
-    None
+    machine.outputs.pop_front()
 }
 
 #[cfg(test)]

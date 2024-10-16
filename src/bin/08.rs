17abb44bbdb08348eb61e29fbf8d052d82271766
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2024-10-16 11:51:14 -0500

    day 8 part 2 (removed nalgebra since there ended up being no need for matrix math

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,5 +1,6 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
-use nalgebra::{Dyn, OMatrix};
 
 advent_of_code::solution!(8);
 
@@ -10,23 +11,27 @@ struct Image {
 
 #[derive(Debug)]
 struct Layer {
-    content: OMatrix<u32, Dyn, Dyn>
+    content: HashMap<(usize, usize), u32>
 }
 
 fn parser(i: &str, (wide, tall): (usize, usize)) -> Image {
     let digits = i.chars().filter_map(|c| c.to_digit(10)).collect::<Vec<u32>>();
     let mut layers = Vec::new();
     for chunk in digits.into_iter().chunks(wide*tall).into_iter() {
-        layers.push(Layer {content: OMatrix::<u32, Dyn, Dyn>::from_row_iterator(wide, tall, chunk)}); 
+        let mut layer = Layer { content: HashMap::new()};
+        for (digit, (row, column)) in chunk.zip((0..tall).cartesian_product(0..wide)) {
+            layer.content.insert((row, column), digit);
+        }
+        layers.push(layer); 
     }
 
     Image { layers }
 }
 
 fn corruption_check(image: Image) -> usize {
-    let fewest_zeroes = image.layers.iter().min_by_key(|l| l.content.iter().filter(|n| **n == 0).count()).unwrap();
+    let fewest_zeroes = image.layers.iter().min_by_key(|l| l.content.values().filter(|n| **n == 0).count()).unwrap();
 
-    fewest_zeroes.content.iter().filter(|n| **n == 1).count() * fewest_zeroes.content.iter().filter(|n| **n == 2).count()
+    fewest_zeroes.content.values().filter(|n| **n == 1).count() * fewest_zeroes.content.values().filter(|n| **n == 2).count()
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
@@ -35,7 +40,32 @@ pub fn part_one(input: &str) -> Option<usize> {
     Some(corruption_check(image))
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
+pub fn part_two(input: &str) -> Option<u32> {
+    let image = parser(input, (25, 6));
+    let mut rendered = HashMap::new();
+
+    for layer in image.layers.iter().rev() {
+        for (location, value) in layer.content.iter() {
+            rendered.entry(location).and_modify(|p| *p = *match value {
+                2 => p,
+                1 | 0 => value,
+                _ => panic!("got a non-expected value"),
+            }).or_insert(*value);
+        }
+    }
+
+    for row in 0..6 {
+        for column in 0..25 {
+            print!("{}", match rendered.get(&(row, column)).unwrap() {
+                1 => "*",
+                0 => " ",
+                _ => panic!("unexpected character"),
+            });
+        }
+        println!();
+    }
+    println!();
+
     None
 }
 

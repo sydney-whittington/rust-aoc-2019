95d02be9cbf9b02f16bcee7a2093a3e0a7133fe0
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2024-10-23 15:49:43 -0500

    day 13 part 2

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,19 +1,25 @@
 use std::collections::HashMap;
 
-use advent_of_code::{execute, parse_machine, Coordinate};
+use advent_of_code::{execute, parse_machine, Coordinate, State};
 use itertools::Itertools;
 
 advent_of_code::solution!(13);
 
-fn render(panel: &HashMap<Coordinate<i64>, i64>) {
-    let (min_x, max_x) = panel.keys().map(|l| l.left).minmax().into_option().unwrap();
-    let (min_y, max_y) = panel.keys().map(|l| l.top).minmax().into_option().unwrap();
+#[allow(dead_code)]
+fn render(screen: &HashMap<Coordinate<i64>, i64>) {
+    let (_min_x, max_x) = screen
+        .keys()
+        .map(|l| l.left)
+        .minmax()
+        .into_option()
+        .unwrap();
+    let (_min_y, max_y) = screen.keys().map(|l| l.top).minmax().into_option().unwrap();
 
-    for top in min_y..=max_y {
-        for left in min_x..=max_x {
+    for top in 0..=max_y {
+        for left in 0..=max_x {
             print!(
                 "{}",
-                match panel.get(&Coordinate { left, top }).unwrap_or(&0) {
+                match screen.get(&Coordinate { left, top }).unwrap_or(&0) {
                     0 => ".",
                     1 => "|",
                     2 => "#",
@@ -26,6 +32,15 @@ fn render(panel: &HashMap<Coordinate<i64>, i64>) {
         println!();
     }
     println!();
+    println!(
+        "score: {}",
+        screen
+            .get(&Coordinate {
+                left: -1_i64,
+                top: 0_i64
+            })
+            .unwrap_or(&-999_i64)
+    );
 }
 
 pub fn part_one(input: &str) -> Option<usize> {
@@ -41,13 +56,42 @@ pub fn part_one(input: &str) -> Option<usize> {
             .or_insert(*id);
     }
 
-    render(&screen);
-
     Some(screen.values().filter(|c| **c == 2).count())
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<i64> {
+    let (_, mut machine) = parse_machine(input).unwrap();
+    let mut screen = HashMap::new();
+
+    machine.program[0] = 2;
+    loop {
+        // will either be waiting on input or terminated
+        let state = execute(&mut machine);
+        // but we need to update the screen either way
+        for (x, y, id) in machine.outputs.iter().tuples() {
+            screen
+                .entry(Coordinate { left: *x, top: *y })
+                .and_modify(|k: &mut i64| *k = *id)
+                .or_insert(*id);
+        }
+
+        // then exit if it's done
+        if matches!(state, State::Terminated) {
+            break;
+        }
+
+        // keep the paddle under the ball
+        let ball_x = screen.iter().find(|(_, v)| **v == 4).unwrap().0.left;
+        let paddle_x = screen.iter().find(|(_, v)| **v == 3).unwrap().0.left;
+
+        machine.inputs.push_back(match ball_x - paddle_x {
+            x if x < 0 => -1,
+            x if x > 0 => 1,
+            _ => 0,
+        });
+    }
+
+    Some(*screen.get(&Coordinate { left: -1, top: 0 }).unwrap())
 }
 
 #[cfg(test)]

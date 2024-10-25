baccf77ab59720e9b2b231d136468f7632b9a13b
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2024-10-25 11:53:50 -0500

    day 14 part 2 failed attempt to be smart

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -55,11 +55,11 @@ fn parser(i: &str) -> IResult<&str, Reactions> {
     ))
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (_, rules) = parser(input).unwrap();
-
-    let mut needed = VecDeque::from([(1, "FUEL")]);
-    let mut leftovers: HashMap<&str, u32> = HashMap::new();
+fn produce<'a>(
+    rules: &'a HashMap<String, Rule>,
+    mut needed: VecDeque<(u32, &'a str)>,
+    leftovers: &mut HashMap<&'a str, u32>,
+) -> u32 {
     let mut ore = 0;
 
     while let Some((mut quantity_needed, chemical)) = needed.pop_front() {
@@ -67,7 +67,7 @@ pub fn part_one(input: &str) -> Option<u32> {
 
         if let Some(stored) = leftovers.get(chemical) {
             if *stored >= quantity_needed {
-                println!("needed {quantity_needed} {chemical} and had it in stock");
+                // println!("needed {quantity_needed} {chemical} and had it in stock");
                 leftovers
                     .entry(chemical)
                     .and_modify(|s| *s -= quantity_needed);
@@ -77,12 +77,12 @@ pub fn part_one(input: &str) -> Option<u32> {
                 leftovers.remove(chemical);
             }
         }
-        print!("need {quantity_needed} {chemical}: from ");
+        // print!("need {quantity_needed} {chemical}: from ");
 
         let reactions_needed = quantity_needed.div_ceil(rule.product.quantity);
         for precursor in rule.precursors.iter() {
             let precursor_needed = reactions_needed * precursor.quantity;
-            print!("{} {}, ", precursor_needed, precursor.symbol);
+            // print!("{} {}, ", precursor_needed, precursor.symbol);
             if precursor.symbol == "ORE" {
                 ore += precursor_needed;
             } else {
@@ -90,7 +90,7 @@ pub fn part_one(input: &str) -> Option<u32> {
             }
         }
         let too_many = (reactions_needed * rule.product.quantity) - quantity_needed;
-        println!(" with {} leftovers", too_many);
+        // println!(" with {} leftovers", too_many);
 
         leftovers
             .entry(&rule.product.symbol)
@@ -98,13 +98,53 @@ pub fn part_one(input: &str) -> Option<u32> {
             .or_insert(too_many);
     }
 
-    dbg!(leftovers);
-
-    Some(ore)
+    ore
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_one(input: &str) -> Option<u32> {
+    let (_, rules) = parser(input).unwrap();
+
+    let needed = VecDeque::from([(1, "FUEL")]);
+    let mut leftovers: HashMap<&str, u32> = HashMap::new();
+
+    let ore_needed = produce(&rules, needed, &mut leftovers);
+    dbg!(leftovers);
+
+    Some(ore_needed)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let (_, rules) = parser(input).unwrap();
+
+    let mut leftovers: HashMap<&str, u32> = HashMap::new();
+    let ore_for_one_fuel = produce(&rules, VecDeque::from([(1, "FUEL")]), &mut leftovers);
+
+    // how many can we make easily?
+    let fast_forward_factor: u64 = 1_000_000_000_000 / ore_for_one_fuel as u64;
+    dbg!(&fast_forward_factor);
+    // it looks like we can be more efficient considering leftovers from the first time
+    leftovers
+        .values_mut()
+        .for_each(|l| *l *= fast_forward_factor as u32);
+    let mut remaining_ore = 1_000_000_000_000 - ore_for_one_fuel as u64 * fast_forward_factor;
+    let mut total_fuel = fast_forward_factor;
+
+    loop {
+        match remaining_ore.checked_sub(produce(
+            &rules,
+            VecDeque::from([(1, "FUEL")]),
+            &mut leftovers,
+        ) as u64)
+        {
+            Some(n) => {
+                remaining_ore = n;
+                total_fuel += 1;
+            }
+            None => {
+                return Some(total_fuel);
+            }
+        }
+    }
 }
 
 #[cfg(test)]
@@ -119,19 +159,33 @@ mod tests {
 
     #[test]
     fn test_part_one_b() {
-        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 1));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some(165));
     }
 
     #[test]
     fn test_part_one_c() {
-        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        let result = part_one(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
         assert_eq!(result, Some(2210736));
     }
 
     #[test]
-    fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+    fn test_part_two_a() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 2,
+        ));
+        assert_eq!(result, Some(460664));
+    }
+
+    #[test]
+    fn test_part_two_b() {
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 3,
+        ));
+        assert_eq!(result, Some(82892753));
     }
 }

fc63771237c002de9d83eeb68e83299028e69b5c
Sydney Whittington <sydney.whittington@mooreintegrity.com>
2024-11-06 14:19:20 -0600

    day 16 fmt and clippy

--- src/bin/16.rs
+++ src/bin/16.rs
@@ -17,7 +17,8 @@ fn phase(nums: Vec<u32>) -> Vec<u32> {
 
         let inputs = nums.iter().zip(mod_pattern).collect_vec();
 
-        let result = inputs.iter()
+        let result = inputs
+            .iter()
             .map(|(a, b)| **a as i32 * **b)
             .sum::<i32>()
             .unsigned_abs()
@@ -33,14 +34,15 @@ fn phase2(nums: VecDeque<u32>) -> VecDeque<u32> {
     let mut output = VecDeque::new();
     output.push_front(*nums.back().unwrap());
 
-    for i in (0..nums.len()-1).rev() {
+    for i in (0..nums.len() - 1).rev() {
         let result = (nums[i] + *output.front().unwrap()) % 10;
 
         output.push_front(result);
     }
 
-    output.into()
+    output
 }
+
 pub fn part_one(input: &str) -> Option<String> {
     let mut digits = input.chars().filter_map(|c| c.to_digit(10)).collect_vec();
 
@@ -54,9 +56,19 @@ pub fn part_one(input: &str) -> Option<String> {
 pub fn part_two(input: &str) -> Option<String> {
     let initial_digits = input.chars().filter_map(|c| c.to_digit(10)).collect_vec();
     let initial_length = initial_digits.len();
-    let offset = initial_digits.iter().take(7).join("").parse::<usize>().unwrap();
+    let offset = initial_digits
+        .iter()
+        .take(7)
+        .join("")
+        .parse::<usize>()
+        .unwrap();
 
-    let mut digits = initial_digits.into_iter().cycle().take(initial_length * 10000).skip(offset).collect();
+    let mut digits = initial_digits
+        .into_iter()
+        .cycle()
+        .take(initial_length * 10000)
+        .skip(offset)
+        .collect();
 
     for _ in 0..100 {
         digits = phase2(digits);
@@ -71,7 +83,10 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let digits = "12345678".chars().filter_map(|c| c.to_digit(10)).collect_vec();
+        let digits = "12345678"
+            .chars()
+            .filter_map(|c| c.to_digit(10))
+            .collect_vec();
         let phase_1 = phase(digits);
         assert_eq!(phase_1.iter().join(""), "48226158".to_string());
         let phase_2 = phase(phase_1);
@@ -84,7 +99,9 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 1));
+        let result = part_two(&advent_of_code::template::read_file_part(
+            "examples", DAY, 1,
+        ));
         assert_eq!(result, Some("84462026".to_string()));
     }
 }
